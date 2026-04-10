35c9cf9f6a286931b27bfafda7a97df3cef2a6e7
miguelmendoza11 <miguel.mendozaj@campusucc.edu.co>
2026-04-10 10:34:42 -0500

    añadir seccion projects

---
+++ components/Projects.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Projects from "@/components/Projects";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-white">
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+    </main>
+  );
+}
