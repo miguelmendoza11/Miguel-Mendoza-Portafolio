a00f6fe562974a4f5d2637112d6f15e0eae41f75
miguelmendoza11 <miguel.mendozaj@campusucc.edu.co>
2026-04-10 10:49:04 -0500

    añadir testimonials a page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Projects from "@/components/Projects";
+import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <About />
       <Projects />
+      <Testimonials />
     </main>
   );
 }
