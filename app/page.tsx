d52758f7e503f27e60586ac6adf66d0588182476
miguelmendoza11 <miguel.mendozaj@campusucc.edu.co>
2026-04-11 09:29:23 -0500

    añadir al page experience

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Projects from "@/components/Projects";
 import Testimonials from "@/components/Testimonials";
+import Experience from "@/components/Experience";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <About />
       <Projects />
       <Testimonials />
+      <Experience />
     </main>
   );
 }
