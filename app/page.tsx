4212831ab66c63386d9761a63f39e2b85aa9d515
miguelmendoza11 <miguel.mendozaj@campusucc.edu.co>
2026-04-21 16:05:07 -0500

    quitar skills

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import Projects from "@/components/Projects";
 import Testimonials from "@/components/Testimonials";
 import Experience from "@/components/Experience";
 import Technologies from "@/components/Technologies";
-import Skills from "@/components/Skills";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import ScrollReveal from "@/components/ScrollReveal";
@@ -30,9 +29,6 @@ export default function Home() {
       </ScrollReveal>
       <ScrollReveal direction="up" delay={100}>
         <Technologies />
-      </ScrollReveal>
-      <ScrollReveal direction="up" delay={100}>
-        <Skills />
       </ScrollReveal>
       <ScrollReveal direction="up" delay={100}>
         <Contact />
